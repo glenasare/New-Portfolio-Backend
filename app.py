a7355940ee045e1f3ae1c7dd8dc2d60923536ed4
Glen Asare <gasare1@asu.edu>
2022-12-16 12:49:01 -0700

    fixed session issues

--- app.py
+++ app.py
@@ -1,20 +1,23 @@
 import hashlib
+import string
 from datetime import datetime, timedelta
-from flask import Flask, request, jsonify, session, make_response
+from flask import Flask, request, jsonify, session, make_response, redirect, render_template
 from flask_jwt_extended import (
     JWTManager, jwt_required,
     get_jwt_identity
 )
 from serverconnection import connect_to_database
 from flask_cors import CORS
-from jwt import decode, encode, ExpiredSignatureError, InvalidTokenError
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from twilio.rest import Client
 
-
-app = Flask(__name__)
-cors = CORS(app, supports_credentials=True)
+app = Flask(__name__, template_folder='templates')
+cors = CORS(app)
 app.config['JWT_SECRET_KEY'] = 'super secret'
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+import random
 
 jwt = JWTManager(app)
 
@@ -35,15 +38,34 @@ def load_user():
         return user
 
 
+class MobileVerificationForm(FlaskForm):
+    mobile_number = StringField('Mobile Number')
+    submit = SubmitField('Send Code')
+
+
+class CodeVerificationForm(FlaskForm):
+    code = StringField('Verification Code')
+    submit = SubmitField('Verify')
+
+
+def generate_verification_code():
+    # generate a random 6-digit verification code
+    return ''.join(random.choices(string.digits, k=6))
+
+
+verification_code = generate_verification_code()
+
+
 @app.route('/', methods=['GET'])
 def home():
-    if "session" in session:
-        token = session["session"]
-        return jsonify({'message': 'You are already logged in', 'token': token})
-    else:
-        resp = jsonify({'message': 'Unauthorized'})
-        resp.status_code = 401
-        return resp
+    conn = connect_to_database()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM userinfo WHERE access_token=%s", (verification_code,))
+    user = cur.fetchone()
+    if not user:
+        return make_response({"message": "Please Login"}, 401)
+    return make_response({"message": "Already Logged In"}, 200)
 
 
 # Function to register a new user
@@ -64,14 +86,14 @@ def register():
     cur = conn.cursor()
 
     # Check if the login name already exists
-    cur.execute("SELECT * FROM users WHERE email=%s", (email,))
+    cur.execute("SELECT * FROM userinfo WHERE email=%s", (email,))
     user = cur.fetchone()
 
     if user is not None:
         return jsonify({"msg": "Login name already exists"}), 400
 
     # Insert the new user into the database
-    cur.execute("INSERT INTO users (first_name, last_name, email, login_name, password) VALUES (%s, %s, %s, %s, %s)",
+    cur.execute("INSERT INTO userinfo (first_name, last_name, email, login_name, password) VALUES (%s, %s, %s, %s, %s)",
                 (first_name, last_name, email, login_name, hashed_password))
     conn.commit()
 
@@ -79,25 +101,14 @@ def register():
     conn.close()
 
 
-def generate_jwt(user_id):
-    try:
-        payload = {
-            "user_id": user_id,
-            "exp": datetime.utcnow() + timedelta(hours=24)
-        }
-
-        jwt_token = encode(payload, app.secret_key, algorithm="HS256")
-
-        return jwt_token
-    except Exception as error:
-        print("Error while generating JWT", error)
+user_id = ""
 
 
 # Function to log in a user
 @app.route('/login', methods=['POST'])
 def login():
     # Check if there is already an access token in the session
-    global resp
+    global resp, user_id
 
     if "session" in session:
         resp = make_response({"message": "User already Logged in"})
@@ -114,19 +125,16 @@ def login():
     hashed_password = hashlib.sha256(password.encode()).hexdigest()
 
     # Check if the username and password are correct
-    cur.execute("SELECT * FROM users WHERE lower(email) = lower(%s) AND ""password=%s", (email, hashed_password))
+    cur.execute("SELECT * FROM userinfo WHERE lower(email) = lower(%s) AND ""password=%s", (email, hashed_password))
     user = cur.fetchone()
 
     if user is None:
         return jsonify({"msg": "Invalid username or password"}), 401
 
     # Create a JSON Web Token with an expiration time of 30 minutes
-
-    jwt_token = generate_jwt(user[0])
-
-    session["session"] = jwt_token
-
-    resp = make_response({"message": "You've been successfully logged in"})
+    if user:
+        user_id = user[0]
+        return redirect("/verify-mobile", 302)
 
     if not user:
         return {"message": "User not found"}, 404
@@ -134,21 +142,76 @@ def login():
     return resp, 200
 
 
-@app.route("/user", methods=["GET"])
-def get_user():
-    jwt_token = session["session"]
+def send_sms(mobile_number, verification_code):
+    # Your Account Sid and Auth Token from twilio.com/console
+    account_sid = 'AC3884413433c29299c4cbf0f6e3094a72'
+    auth_token = 'b54f592addbd493d293b9b109e6ed4a9'
+    client = Client(account_sid, auth_token)
 
-    try:
-        payload = decode(jwt_token, app.secret_key, algorithms="HS256")
-        user_id = payload["user_id"]
-    except:
-        if InvalidTokenError:
-            return {"message": "Invalid Toke"}
-        if ExpiredSignatureError:
-            return {"message": "Token Expired Login Again"}
+    message = client.messages.create(
+        body="Your verification code is: " + verification_code,
+        from_='+12059474196',
+        to=mobile_number
+    )
+
+
+@app.route("/verify-mobile", methods=['GET', 'POST'])
+def verify_mobile():
+    global verification_code
+    form = MobileVerificationForm()
+
+    if form.validate_on_submit():
+        mobile_number = form.mobile_number.data
+
+        if mobile_number:
+            send_sms(mobile_number, verification_code)
+            return redirect("/verify-code", code=302)
+
+    return render_template('verify_mobile.html', form=form)
+
+
+@app.route("/verify-code", methods=["GET", 'POST'])
+def verify_code():
+    form = CodeVerificationForm()
+    entered_code = form.code.data
 
     conn = connect_to_database()
     cur = conn.cursor()
+    if form.validate_on_submit():
+
+        # compare entered code with the generated code to verify mobile number
+        if entered_code == verification_code:
+            # mobile number is verified
+
+            access_code = verification_code
+
+            cur.execute("SELECT * FROM userinfo WHERE access_token=%s", (verification_code,))
+            user = cur.fetchone()
+            if user is None:
+                cur.execute("UPDATE userinfo SET access_token = %s where id = %s ", (access_code,user_id))
+                conn.commit()
+            else:
+                cur.execute("UPDATE userinfo SET access_token = %s where id=%s ", (access_code, user_id))
+                conn.commit()
+
+            return redirect("http://glenasare.com/", 200)
+        else:
+            # entered code is incorrect
+            return "Incorrect verification code"
+    return render_template('verify_code.html', form=form)
+
+
+@app.route("/user", methods=["GET"])
+def get_user():
+    # print(user_info)
+    # return str(user_info)
+    conn = connect_to_database()
+    cur = conn.cursor()
+
+    cur.execute("SELECT * FROM userinfo WHERE access_token=%s", (verification_code,))
+    user = cur.fetchone()
+    if not user:
+        return make_response({"message"}, "Invalid Token Please Login", 401)
 
     cur.execute(
         "SELECT id, email, first_name, last_name FROM users WHERE id = %s", (user_id,)
@@ -175,10 +238,15 @@ def protected():
 
 @app.route('/logout', methods=['POST', 'GET'])
 def logout():
+    conn = connect_to_database()
+    cur = conn.cursor()
+
+    userid = str(user_id)
+
+    cur.execute("UPDATE userinfo SET access_token = null where id = %s ", (userid,))
+    conn.commit()
+
     resp = make_response({"message": "You have been logged out successfully"}, 202)
-    resp.delete_cookie('session')
-    session.pop('session', None)
-    session.clear()
 
     return resp
 
